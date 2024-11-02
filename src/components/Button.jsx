3c17c1336f655451129763a59042a2d61599b08a
Tasfiq-asif <tasfiq.asif.23@gmail.com>
2024-11-02 20:59:40 +0600

    button component added

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({children,onClick,variant='primary'}) => {
+    const baseStyle ="px-4 py-2 rounded font-semibold text-sm transition-all duration-200"
+    const variants ={
+        primary: "bg-primary text-background hover:bg-secondary",
+        secondary: "bg-secondary text-background hover:bg-primary",
+        outline:"border border-primary text-primary hover:bg-background"
+    }
+  return (
+    <button className={`${baseStyles} ${variants[variant]}`} onClick={onClick}>
+    {children}
+  </button>
+  )
+}
+
+export default Button
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: '#EEEBDD',
+        richRed: '#810000',
+        deepMaroon: '#630000',
+        almostBlack: '#1B1717',
+      },
+    },
   },
   plugins: [],
 }
