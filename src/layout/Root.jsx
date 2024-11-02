ec4471e1b0eed704d07279a542ab636d90c04a0a
Tasfiq-asif <tasfiq.asif.23@gmail.com>
2024-11-02 22:47:51 +0600

    Browser router dom created

---
+++ src/layout/Root.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer/Footer'
+
+const Root = () => {
+  return (
+    <div className='max-w-7xl mx-auto' >
+        <Navbar/>
+        <div className="min-h-[calc(100vh-306px)]">
+            <Outlet/>
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Root
