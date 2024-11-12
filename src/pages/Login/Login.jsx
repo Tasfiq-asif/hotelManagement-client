84c0a7b7f874040adf2eb62b464262285485e2fb
Tasfiqul Asif <tasfiq.asif.23@gmail.com>
2024-11-12 20:39:50 +0600

    test

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -56,8 +56,7 @@ const Login = () => {
     <div className="text-center lg:text-left">
       <h1 className="text-5xl font-bold">Login now!</h1>
       <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+        Please Log IN
       </p>
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
